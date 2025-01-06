0996924e77c6c9683b393191e95147276a6986d8
harvey <22130158@st.hcmuaf.edu.vn>
2025-01-07 02:53:26 +0700

    create mail properties

---
+++ Ecommerce_Final/src/main/java/com/example/ecommerce/mail/MailProperties.java
@@ -0,0 +1,49 @@
+package com.example.ecommerce.mail;
+
+import java.util.Properties;
+import java.io.IOException;
+
+public class MailProperties {
+    public static final Properties prop = new Properties();
+
+    static {
+        try {
+            prop.load(MailProperties.class.getClassLoader().getResourceAsStream("mail.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Properties getSMTPPro() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", getMailHost());
+        properties.put("mail.smtp.port", getMailPort());
+        properties.put("mail.smtp.auth", getMailAuth());
+        properties.put("mail.smtp.starttls.enable", getMailStarttls());
+        return properties;
+    }
+
+    public static String getMailHost() {
+        return prop.get("mail.smtp.host").toString();
+    }
+
+    public static String getMailPort() {
+        return prop.get("mail.smtp.port").toString();
+    }
+
+    public static String getMailAuth() {
+        return prop.get("mail.smtp.auth").toString();
+    }
+
+    public static String getMailStarttls() {
+        return prop.get("mail.smtp.starttls.enable").toString();
+    }
+
+    public static String getEmail() {
+        return prop.get("mail.username").toString();
+    }
+
+    public static String getPassword() {
+        return prop.get("mail.password").toString();
+    }
+}
