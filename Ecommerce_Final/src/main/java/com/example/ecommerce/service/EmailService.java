87a825cf123d2e0f1d856673e76bd6f67bf464e3
harvey <22130158@st.hcmuaf.edu.vn>
2025-01-07 14:34:23 +0700

    handle send email

--- Ecommerce_Final/src/main/java/com/example/ecommerce/service/EmailService.java
+++ Ecommerce_Final/src/main/java/com/example/ecommerce/service/EmailService.java
@@ -1,10 +1,53 @@
 package com.example.ecommerce.service;
 
 import com.example.ecommerce.DAO.interf.IJavaMail;
+import com.example.ecommerce.mail.MailProperties;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 public class EmailService implements IJavaMail {
     @Override
-    public boolean send(String to, String subject, String message) {
-        return false;
+    public boolean send(String to, String subject, String messageContent) {
+        // Get properties object
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", MailProperties.HOST_NAME);
+        props.put("mail.smtp.socketFactory.port", MailProperties.SSL_PORT);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.port", MailProperties.SSL_PORT);
+
+        // get Session
+        Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(MailProperties.APP_EMAIL, MailProperties.APP_PASSWORD);
+            }
+        });
+
+        // compose message
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(messageContent);
+
+            // send message
+            Transport.send(message);
+
+            System.out.println("Message sent successfully");
+            return true;
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String to = "22130158@st.hcmuaf.edu.vn";
+        String subject = "hello";
+        String message = "ohayo";
+        IJavaMail emailService = new EmailService();
+        System.out.println(emailService.send(to, subject, message));
     }
 }
