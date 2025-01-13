e646df445224e36dacb816d9330e52ab815d31c0
harvey <22130158@st.hcmuaf.edu.vn>
2025-01-13 17:17:37 +0700

    read jsonfile vietnam.json

--- Ecommerce_Final/src/main/java/com/example/ecommerce/Location/LocationData.java
+++ Ecommerce_Final/src/main/java/com/example/ecommerce/Location/LocationData.java
@@ -1,4 +1,45 @@
 package com.example.ecommerce.Location;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class LocationData {
+    public static void main(String[] args) {
+        String content = readFile("D:\\DangTranTanLuc\\WebProgramming\\Ecommerce_Final\\src\\main\\webapp\\public\\json\\vietnam.json");
+
+        JSONObject jsonObject = new JSONObject(content);
+        JSONArray provinces = jsonObject.getJSONArray("province");
+        System.out.println("Provinces:");
+        for (int i = 0; i < provinces.length(); i++) {
+            JSONObject province = provinces.getJSONObject(i);
+            int idProvince = province.getInt("idProvince");
+            String name = province.getString("name");
+            System.out.println("ID: " + idProvince + ", Name: " + name);
+        }
+
+        JSONArray districts = jsonObject.getJSONArray("district");
+        System.out.println("\nDistricts:");
+        for (int i = 0; i < districts.length(); i++) {
+            JSONObject district = districts.getJSONObject(i);
+            int idDistrict = district.getInt("idDistrict");
+            int idProvince = district.getInt("idProvince");
+            String name = district.getString("name");
+            System.out.println("ID: " + idDistrict + ", Province ID: " + idProvince + ", Name: " + name);
+        }
+    }
+
+    private static String readFile(String filePath) {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            return new String(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
